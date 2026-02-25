49d8bce80592d4e0ad416bbf2c67576540a786a0
romncix999 <zingox80@gmail.com>
2026-02-25 00:48:32 +0000

    Create APPS_DATA with app information and categories
    Added APPS_DATA object containing app details and categories.

---
+++ api/lien.js
@@ -0,0 +1,35 @@
+const APPS_DATA = {
+  "apps": [
+    {
+      "id": 1,
+      "name": "YouTube",
+      "category": "Video",
+      "description": "منصة مشاركة الفيديو الأكبر في العالم",
+      "url": "https://www.youtube.com",
+      "icon": "video",
+      "color": "#FF0000",
+      "rating": 4.8,
+      "downloads": "10B+"
+    },
+    {
+      "id": 2,
+      "name": "GitHub",
+      "category": "Development",
+      "description": "منصة استضافة الكود المصدري والتعاون البرمجي",
+      "url": "https://www.github.com",
+      "icon": "code",
+      "color": "#ffffff",
+      "rating": 4.9,
+      "downloads": "500M+"
+    }
+  ],
+  "categories": [
+    "الكل",
+    "Video",
+    "Development",
+    "Social",
+    "Tools",
+    "News"
+  ],
+  "featured": [1, 2]
+};
